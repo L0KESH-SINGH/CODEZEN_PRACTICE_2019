76e23a52b0788046a40286a27f7a23c6e2d9b85b
lokiSpy <lokeshsingh.singh2@gmail.com>
2019-06-05 22:35:16 +0530

    question solved

---
+++ src/Arrays/Special_sum_of_array.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+public class Special_sum_of_array {
+	
+	public static int sum(int arr[])
+	{
+         //Write code here 
+        int temp = 0 ; 
+        int sum = 0 ;
+        for(int i = 0 ; i<arr.length ; i++)
+        {
+            sum += arr[i] ;
+            if(sum>9)
+            {
+                temp = sum%10 ;
+                sum = sum/10 ;
+                temp += sum ;
+                sum = temp ;
+			}
+		}
+        return sum ;
+	}
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
