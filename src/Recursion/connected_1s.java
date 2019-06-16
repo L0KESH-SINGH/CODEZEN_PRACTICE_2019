e72f8659761540c83e747bee0d6bb51ffcbb157f
lokiSpy <lokeshsingh.singh2@gmail.com>
2019-06-16 13:16:13 +0530

    final recursion

--- src/Recursion/connected_1s.java
+++ src/Recursion/connected_1s.java
@@ -5,30 +5,20 @@ import java.util.Scanner;
 public class connected_1s {
 	
 	public static int[] a = { 0,1,1,-1,-1,0,-1,1} ;
-	public static int[] b = { 1,0,1,-1,0,-1,1,-1} ;
+	public static int[] b = { 1,0,1,-1,0,-1,1,-1} ;	
 	
-	public static int getMaxOnes(int[][] inputMatrix, int i , int j){
-		
-		if(i>inputMatrix.length-1 || i<0 || j<0 || j>inputMatrix[i].length-1)
-		{
-			return 0 ;
+	public static int getMaxOnes(int[][] inputMatrix, int i, int j) {
+
+		if (i >= inputMatrix.length || i < 0 || j < 0 || j >= inputMatrix[i].length || inputMatrix[i][j] == 0) {
+			return 0;
 		}
-		int x = inputMatrix[i][j] ;
-		int max = 0 ;
-		if(x==1)
-		{
-			inputMatrix[i][j] = 0 ;
-			for(int p=0 ; p<8 ; p++)
-			{
-				int t = 1 + getMaxOnes(inputMatrix, i+a[p], j+b[p]) ;
-				if(t>max)
-				{
-					max = t ;
-				}
-			}
-			return max ;
+		int max = 1 ;
+		inputMatrix[i][j] = 0;
+		for (int p = 0; p < 8; p++) {
+			max += getMaxOnes(inputMatrix, i + a[p], j + b[p]);
 		}
-		return max ;
+		return max;
+
 	}
 	
 	public static void main(String[] args) {
@@ -44,7 +34,19 @@ public class connected_1s {
 				arr[i][j] = s.nextInt() ;
 			}
 		}
-		System.out.println(getMaxOnes(arr, 0, 0));
+		int bigmax=Integer.MIN_VALUE ;
+		for(int i=0 ; i<n ; i++)
+		{
+			for(int j=0 ; j<m ; j++)
+			{
+				if(arr[i][j]==1)
+				{
+					int temp = getMaxOnes(arr, i, j);
+					bigmax = Math.max(bigmax, temp) ;
+				}
+			}
+		}
+		System.out.println(bigmax);
 	}
 
 }
